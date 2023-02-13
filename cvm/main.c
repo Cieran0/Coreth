f689454d736d87ce20b0927559e8dd50c1242ede
Cieran O'Neill <cieran999@gmail.com>
2023-02-13 23:04:52 +0000

    Added token_type enum.

--- cvm/main.c
+++ cvm/main.c
@@ -2,18 +2,31 @@
 #include "cvm.h"
 #include "variable.h"
 #include "function.h"
-#include <unistd.h>
+#include "token_type.h"
 
 #define MAX_FUNCTION_COUNT 0xFFF
 
-u8 bytes[] = {8,0,16,0,0,0,0,0,16,0,0,0,0,0,0,0};
-u8* ptr = bytes;
+u8* bytes;
+u64 length; 
+u8* ptr;
+
+void read_file(char* path) {
+    FILE *f = fopen(path, "rb+");
+    if (f)
+    {
+        fseek(f, 0L, SEEK_END);
+        long filesize = ftell(f); // get file size
+        fseek(f, 0L ,SEEK_SET); //go back to the beginning
+        u8* bytes = (u8*)malloc(filesize); // allocate the read buf
+        fread(bytes, 1, filesize, f);
+        fclose(f);
+    }
+}
 
 int main(int argc, char const *argv[])
 {
     variable* mem = NULL;
     function functions[MAX_FUNCTION_COUNT] = {0};
-    functions[0] = new_function(next_u32(&ptr));
     printf("%li\n",sizeof(functions));
     return 0;
 }
