18c7efc7a07beb926dc987bc8ff2ed7680dbf64c
Cieran O'Neill <cieran999@gmail.com>
2023-02-13 21:28:46 +0000

    Added some helpers for byte arrays.

--- parser/CVMifier.java
+++ parser/CVMifier.java
@@ -1,5 +1,7 @@
 package parser;
 
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map.Entry;
@@ -17,6 +19,33 @@ public class CVMifier implements Serializer{
                 return i;
         }
         return -1;
+    }
+
+    public byte[] combineBytes(byte[] array1, byte[] array2) {
+        byte[] newByteArray = new byte[array1.length+array2.length];
+        for (int i = 0; i < array1.length; i++) {
+            newByteArray[i]=array1[i];
+        }
+        for (int i = 0; i < array2.length; i++) {
+            newByteArray[i+array1.length]=array2[i];
+        }
+        return newByteArray;
+    }
+
+    public byte[] longToByteArray(long l) {
+        return ByteBuffer.allocate(8).order(ByteOrder.LITTLE_ENDIAN).putLong(l).array();
+    }
+
+    public byte[] intToByteArray(int i) {
+        return ByteBuffer.allocate(4).order(ByteOrder.LITTLE_ENDIAN).putInt(i).array();
+    }
+
+    public byte[] shortToByteArray(short s) {
+        return ByteBuffer.allocate(2).order(ByteOrder.LITTLE_ENDIAN).putShort(s).array();
+    }
+
+    public byte[] byteToByteArray(byte b) {
+        return new byte[]{b};
     }
 
     public void save(String path) {
